141ecd10c45ffb7de32a670c89622730f530925b
Soudip Panja <soudip03panja@gmail.com>
2025-10-03 16:21:32 +0530

    fix: db changed.

--- db/db.connect.js
+++ db/db.connect.js
@@ -3,12 +3,27 @@ require("dotenv").config();
 
 const mongoUrl = process.env.MONGODB;
 
+// const initializeDatabase = async () => {
+//   await mongoose
+//     .connect(mongoUrl)
+//     .then(() => {
+//       console.log("Connected to database.");
+//     })
+//     .catch((error) => console.log("Error in connecting to database.", error));
+// };
+
 const initializeDatabase = async () => {
-  await mongoose
-    .connect(mongoUrl)
-    .then(() => {
-      console.log("Connected to database.");
-    })
-    .catch((error) => console.log("Error in connecting to database.", error));
+  try {
+    const connection = await mongoose.connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    if (connection) {
+      console.log("connected succesfully");
+    }
+  } catch (error) {
+    console.log("connection failed", error);
+  }
 };
+
 module.exports = { initializeDatabase };
